c81f4e2b110ba8e60f51bac49d5d903c8b6d9aac
Hiroaki Nakamura <hnakamur@gmail.com>
2012-03-24 22:42:21 +0900

    use IO.select and read_nonblock

--- a.sh
+++ a.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #read line
 #echo $line
-echo -n hi,
-sleep 3
+#echo -n hi,
+#sleep 3
 echo there.
 echo line 1
 echo line 2
 echo line 3
 echo line 4
 echo message to stderr 1>&2
+echo line 5
 #exit 1
--- pipe1.rb
+++ pipe1.rb
@@ -3,10 +3,12 @@
 class Command
   attr_reader :info
   attr_reader :history
+  attr_reader :outdata, :errdata
 
   def initialize ()
     @info = {}
     @history = []
+    @data = []
   end
 
   def do(*elem)
@@ -18,47 +20,71 @@ class Command
     rp2, wp2 = IO.pipe
     pid = Process.fork do
       wp0.close
-      rp1.close
-      rp2.close
       STDIN.reopen(rp0)
-      STDOUT.reopen(wp1)
-      STDERR.reopen(wp2)
-      exec line
       rp0.close
+
+      rp1.close
+      STDOUT.reopen(wp1)
       wp1.close
+
+      rp2.close
+      STDERR.reopen(wp2)
       wp2.close
+
+      STDOUT.sync = STDERR.sync = true
+
+      exec *elem
     end
 
-    wp0.puts "hello!"
-    wp0.close
+#    wp0.puts "hello!"
+    rp0.close
     wp1.close
     wp2.close
-    stdout = rp1.read
-    stderr = rp2.read
+    done = false
+    until done
+      rs, ws = IO.select([rp1, rp2])
+      rs.each{|r|
+        begin
+          ret = r.read_nonblock 4096
+          if r == rp1
+            @outdata = ret
+          elsif r == rp2
+            @errdata = ret
+          end
+        rescue EOFError
+          done = true
+        end
+      }
+    end
+#    @outdata = rp1.read
+#    @errdata = rp2.read
     rp1.close
     rp2.close
 
     status = Process.waitpid2.last
-    @info = { :line => line, :pid => pid, :status => status,
-      :exitstatus => status.exitstatus, :time => time,
-      :stdout => stdout.chomp, :stderr => stderr.chomp }
-    @history.push(@info)
+#    @info = { :line => line, :pid => pid, :status => status,
+#      :exitstatus => status.exitstatus, :time => time,
+#      :stdout => stdout.chomp, :stderr => stderr.chomp }
+#    @history.push(@info)
   end
 end
 
 cmd = Command.new()
 cmd.do("./a.sh")
+puts "outdata=#{cmd.outdata}"
+puts "errdata=#{cmd.errdata}"
+
 #cmd.do("ls -l", "/etc")
 #cmd.do("date")
 #cmd.do("ls -l noExistFile")
 
-print "=== command history ===\n"
-cmd.history.each do |h|
-  print "time => ", h[:time], "\n"
-  print "line => ", h[:line], "\n"
-  print "exitstatus => ", h[:exitstatus], "\n"
-  print "status => ", h[:status], "\n"
-  print "stdout => ", h[:stdout], "\n"
-  print "stderr => ", h[:stderr], "\n"
-  print "-----\n\n"
-end
+#print "=== command history ===\n"
+#cmd.history.each do |h|
+#  print "time => ", h[:time], "\n"
+#  print "line => ", h[:line], "\n"
+#  print "exitstatus => ", h[:exitstatus], "\n"
+#  print "status => ", h[:status], "\n"
+#  print "stdout => ", h[:stdout], "\n"
+#  print "stderr => ", h[:stderr], "\n"
+#  print "-----\n\n"
+#end
